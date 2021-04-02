58ee1bf45ebe0fa0c8519d205d25899ae93831e2
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-02 15:13:43 +0300

    Adding cookie and deleting cookie

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+// don't do this in the project
+app.get('/setCookie/:clr', ((req, res) => {
+  res.cookie('color', req.params.clr, {httpOnly: true}).send('cookie set');
+}))
+
+// don't do this in the project
+app.get('/deleteCookie', ((req, res) => {
+  res.clearCookie('color');
+  res.send('cookie deleted');
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
